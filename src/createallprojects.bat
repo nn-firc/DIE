76eaddd8e67e22864ff26f4a45b0a7eb9a6f278d
nn-firc <2319221757@qq.com>
2023-03-10 15:54:53 +0800

    [VPC]use game project

--- src/createallprojects.bat
+++
@@ -1,3 +0,0 @@
-pushd %~dp0
-devtools\bin\vpc.exe /csgo /win32 +csgo_partner /mksln csgo_partner.sln
-popd
---
+++ src/creategameprojects.bat
@@ -0,0 +1,3 @@
+pushd %~dp0
+devtools\bin\vpc.exe /csgo /win32 +game /mksln csgo_partner.sln
+popd
